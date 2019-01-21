23799a7de02540150c8bbef6856791a024985ca8
Ali Zahid <ali.zahid@live.com>
2019-01-21 15:30:53 +0400

    Bug fix

--- src/store.js
+++ src/store.js
@@ -6,9 +6,8 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const persistConfig = {
-  key: 'root',
   storage,
-  whitelist: ['foo']
+  key: 'root'
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
