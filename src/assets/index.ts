1dba07b49b30f9c3e0e3fe478d2cc498f8b4f91e
Ali Zahid <ali.zahid@live.com>
2019-11-27 07:33:22 +0400

    Sort icons added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,7 @@
 export { default as img_left } from './left.png'
 export { default as img_link } from './link.png'
+export { default as img_sort_down } from './sort_down.png'
+export { default as img_sort_up } from './sort_up.png'
 
 export { default as img_border } from './border.png'
 
--- src/components/sort-comments.tsx
+++ src/components/sort-comments.tsx
@@ -1,7 +1,7 @@
-import { lowerCase, startCase } from 'lodash'
 import React, { FunctionComponent } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
+import { img_sort_down, img_sort_up } from '../assets'
 import { colors, fonts, layout } from '../styles'
 import { SortField, SortOrder } from '../types'
 import { Touchable } from './touchable'
@@ -23,18 +23,27 @@ export const SortComments: FunctionComponent<Props> = ({
       onPress={() =>
         onChange(sortField === 'date' ? 'rating' : 'date', sortOrder)
       }>
-      <Text style={styles.label}>{startCase(sortField)}</Text>
+      <Text style={styles.label}>Sort by {sortField}</Text>
     </Touchable>
     <Touchable
       onPress={() => onChange(sortField, sortOrder === 'asc' ? 'desc' : 'asc')}>
-      <Text style={styles.label}>{lowerCase(sortOrder)}</Text>
+      <Image
+        style={styles.icon}
+        source={sortOrder === 'asc' ? img_sort_up : img_sort_down}
+      />
     </Touchable>
   </View>
 )
 
 const styles = StyleSheet.create({
+  icon: {
+    height: 20,
+    margin: layout.margin,
+    width: 20
+  },
   label: {
     ...fonts.small,
+    color: colors.accent,
     margin: layout.margin
   },
   main: {
